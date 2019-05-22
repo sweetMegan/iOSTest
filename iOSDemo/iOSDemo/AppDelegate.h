25bc6bb9675c9f596e42cd97241d62fed07de875
sweetMegan <sweetsmelon@163.com>
2019-05-22 13:51:55 +0800

    init

---
+++ iOSDemo/iOSDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSDemo
+//
+//  Created by mingsha on 2019/5/22.
+//  Copyright © 2019 mingsha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSDemo/iOSDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSDemo
+//
+//  Created by mingsha on 2019/5/22.
+//  Copyright © 2019 mingsha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
